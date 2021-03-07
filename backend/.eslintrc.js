a8f34c34bfb7c6673b79683d1f82910416ac4b5b
Luis <luisbch9@gmail.com>
2021-03-06 22:35:54 -0500

    Delete Backend

--- backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    node: true,
-  },
-  extends: ["eslint:recommended", "prettier", "plugin:prettier/recommended"],
-  parser: "@babel/eslint-parser",
-  plugins: ["prettier"],
-  rules: { "prettier/prettier": "warn" },
-};
--- backend/src/config.js
+++
@@ -1,8 +0,0 @@
-import { config } from "dotenv";
-config();
-
-const database = {
-  mongodbUri: process.env.MONGODB_URI,
-};
-
-export { database };
